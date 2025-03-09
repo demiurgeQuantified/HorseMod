c2c17b386a543f3f8168c641b04c2ba2292007e8
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-03-09 04:39:31 +0000

    add speed gene

---
+++ Contents/mods/HorseMod/42/media/lua/shared/HorseMod/genes.lua
@@ -0,0 +1,17 @@
+-- these values may need adjusting
+AnimalGenomeDefinitions.genes["speed"] = {
+    minValue = 0.8,
+    maxValue = 1.2
+}
+
+-- TODO: unimplemented
+AnimalGenomeDefinitions.genes["stamina"] = {
+    minValue = 0.7,
+    maxValue = 1.4
+}
+
+-- TODO: unimplemented
+AnimalGenomeDefinitions.genes["carryWeight"] = {
+    minValue = 0.7,
+    maxValue = 1.4
+}
--- Contents/mods/HorseMod/42/media/lua/shared/HorseMod/horses.lua
+++ Contents/mods/HorseMod/42/media/lua/shared/HorseMod/horses.lua
@@ -1,8 +1,24 @@
 local HorseUtils = require("HorseMod/HorseUtils")
 
+-- we wait a tick to process animals because their type isn't set when the event triggers
+
+---@type IsoAnimal[]
+local animalsToProcess = table.newarray()
+
 Events.OnCreateLivingCharacter.Add(function(character, desc)
-    if character:isAnimal() and HorseUtils.isHorse(character --[[@as IsoAnimal]]) then
+    if character:isAnimal() then
+        table.insert(animalsToProcess, character)
         ---@cast character IsoAnimal
-        character:setVariable("isHorse", true)
+    end
+end)
+
+Events.OnTick.Add(function()
+    for i = #animalsToProcess, 1, -1  do
+        local animal = animalsToProcess[i]
+        if HorseUtils.isHorse(animal) then
+            animal:setVariable("isHorse", true)
+            animal:setVariable("geneSpeed", animal:getUsedGene("speed"):getCurrentValue())
+        end
+        animalsToProcess[i] = nil
     end
 end)
