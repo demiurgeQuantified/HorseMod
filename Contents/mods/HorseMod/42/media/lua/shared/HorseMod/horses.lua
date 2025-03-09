5f74c6300cb786273096c70694c70569a1eec7d4
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-03-09 02:18:36 +0000

    add horse check before setting horse var

--- Contents/mods/HorseMod/42/media/lua/shared/HorseMod/horses.lua
+++ Contents/mods/HorseMod/42/media/lua/shared/HorseMod/horses.lua
@@ -1,5 +1,7 @@
+local HorseUtils = require("HorseMod/HorseUtils")
+
 Events.OnCreateLivingCharacter.Add(function(character, desc)
-    if character:isAnimal() then
+    if character:isAnimal() and HorseUtils.isHorse(character --[[@as IsoAnimal]]) then
         ---@cast character IsoAnimal
         character:setVariable("isHorse", true)
     end
